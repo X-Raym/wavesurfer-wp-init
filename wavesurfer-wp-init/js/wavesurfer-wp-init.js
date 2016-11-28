88c547dbee8511c6e2284403df784649b36526d5
Raymond Radet <X-Raym@users.noreply.github.com>
2016-11-28 23:01:34 +0100

    Update wavesurfer-wp-init.js

--- wavesurfer-wp-init/js/wavesurfer-wp-init.js
+++ wavesurfer-wp-init/js/wavesurfer-wp-init.js
@@ -4,6 +4,14 @@
  
 var $j = jQuery.noConflict();
 
+
+// Initialize WaveSurfer Player at page ready
+$j(document).on('ready', function() {
+    $j(document).trigger('wavesurfer-wp-init')
+});
+
+
+// Initialize WaveSurfer Player after a button click
 $j('.wavesurfer-button').click(function() {
     $j(document).trigger('wavesurfer-wp-init')
 });
