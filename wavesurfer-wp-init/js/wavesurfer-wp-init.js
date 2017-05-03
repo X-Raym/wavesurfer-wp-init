f5a792cb989530d87ff0fa181feb2fb78904cae4
Raymond Radet <X-Raym@users.noreply.github.com>
2017-05-03 19:19:00 +0200

    Update wavesurfer-wp-init.js

--- wavesurfer-wp-init/js/wavesurfer-wp-init.js
+++ wavesurfer-wp-init/js/wavesurfer-wp-init.js
@@ -8,6 +8,9 @@
 var $j = jQuery.noConflict();
 
 
+// ====== User-Defined Events ====== //
+// Delete unnecessary ones.
+
 // Page ready
 $j(document).on('ready', function() {
     $j(document).trigger('wavesurfer-wp-init')
@@ -26,8 +29,7 @@ $(function(){
   });
 });
 
-
-// AJAX Load More Plugin
+// AJAX Load More Plugin: https://connekthq.com/plugins/ajax-load-more/docs/callback-functions/
 $(function() {
   $.fn.almComplete = function(alm){
     $j(document).trigger('wavesurfer-wp-init');
